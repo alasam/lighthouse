c61cf71cfecdb3fb2f5a97b39f1d0b3eb56be552
Anthony <alasam@live.com>
2021-12-08 06:21:19 -0500

    creat app, add compoments and some functionality

---
+++ w7/fancy-buttons/src/components/AngryButton.js
@@ -0,0 +1,16 @@
+import React from 'react'; //optional
+import { useState } from "react";
+
+function AngryButton(props) {
+  const [anger, setAnger] = useState(0);
+
+  
+  return (
+    <button style={{ backgroundColor: `rgba(255,0,0,${anger})` }} className="AngryButton">
+      {anger < 1 && <span>Don't click me too much! </span>}
+      {anger > 1 && <span>Rawr!</span>}
+    </button>
+  );
+}
+
+export default AngryButton;
---
+++ w7/fancy-buttons/src/components/CounterButton.js
@@ -0,0 +1,13 @@
+import React from 'react'; //optional
+import { useState } from "react";
+
+function CounterButton(props){
+  const [clickAmount, setClickAmount] = useState(0);
+  return (
+    <button className="CounterButton">
+    You clicked me {clickAmount} times
+  </button>
+  );
+}
+
+export default CounterButton;
