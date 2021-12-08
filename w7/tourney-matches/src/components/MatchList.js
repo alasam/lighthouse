6014a51c2b9a6a30b3e4f00536c402dc81a6f335
Anthony <alasam@live.com>
2021-12-08 02:24:22 -0500

    React Tournament-Matches practice walkthrough

---
+++ w7/tourney-matches/src/components/MatchList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Match from "./Match";
+import matchData from '../data/matchData';
+
+
+function MatchList(props) {
+  const { matchData } = props;
+  const oneMatch = matchData[0];
+  const parsedMatches = matchData.map(match => <Match key={match.matchNumber} {...match} />);
+  return (
+    <section className="PlayerList MatchList">
+      <h1>Match list</h1>
+      {parsedMatches}
+    </section>
+  );
+}
+export default MatchList;
---
+++ w7/tourney-matches/src/components/PlayerList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Player from "./Player";
+import playerData from '../data/playerData';
+import matchData from '../data/matchData';
+import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers';
+
+function PlayerList(props) {
+  const preparedPlayerData = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayers(preparedPlayerData, matchData);  
+  const onePlayer = parsedPlayerData[0];
+  const parsedPlayers = parsedPlayerData.map(player => <Player key={player.gamerTag} {...player} />);
+  return (
+    <section className="PlayerList">
+      <h1>Current participating players</h1>
+      { parsedPlayers }
+    </section>
+  );
+}
+export default PlayerList;
